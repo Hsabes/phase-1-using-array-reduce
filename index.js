24e040e5639ea8e0af3aaa0a500afe2731ee2c10
Harrison Sabean <hsabes@gmail.com>
2022-05-24 21:59:56 -0700

    commit tree 2b483853b965b9412df6560640c3f57b38894baa

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// let totalBatteries = (array, init) => {
+//     return array.reduce((previousValue, currentValue) => (previousValue + currentValue), init);
+// };
+
+// console.log(totalBatteries(batteryBatches, 0));
+
+let totalBatteries = batteryBatches.reduce((previousValue, currentValue) => (previousValue + currentValue), 0);
+
+console.log(totalBatteries);
+
+// function batteries(array, init){
+//     let totalBatteries = array.reduce(
+//         (previousValue, currentValue) => previousValue + currentValue, init)
+//     return totalBatteries;
+// };
+
+// console.log(batteries(batteryBatches, 0));
